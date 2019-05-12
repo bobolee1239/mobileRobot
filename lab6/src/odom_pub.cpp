5a7dfb94343a8ce956a193c3324740dc980aa352
Brian Lee <bobolee1239@gmail.com>
2019-05-12 13:33:42 +0800

    revise format

--- lab6/src/odom_pub.cpp
+++ lab6/src/odom_pub.cpp
@@ -3,41 +3,40 @@
 #include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
 
-double x=0.0;
-double y=0.0;
-double th=0.0;
-double vx=0.0;
-double vy=0.0;
-double vth=0.0;
+double x   = 0.0;
+double y   = 0.0;
+double th  = 0.0;
+double vx  = 0.0;
+double vy  = 0.0;
+double vth = 0.0;
+
 const float pi = 3.1415926;
 
-void callback(const geometry_msgs::Twist& feedbackSpeed)
-{ 
+void callback(const geometry_msgs::Twist& feedbackSpeed) {
   ROS_INFO("x=%f, y=%f, th=%f\n", x, y, th*180/pi);
-  vx=feedbackSpeed.linear.x;
-  vy=feedbackSpeed.linear.y;
-  vth=feedbackSpeed.angular.z;
+  vx  = feedbackSpeed.linear.x;
+  vy  = feedbackSpeed.linear.y;
+  vth = feedbackSpeed.angular.z;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char *argv[]) {
   ros::init(argc, argv, "odometry_publisher");
 
   ros::NodeHandle np, ns;
-  
-  ros::Subscriber vel_sub = ns.subscribe("feedback_Vel", 50, callback);	//feedback_wheel_speed
+
+  //  feedback_wheel_speed
+  ros::Subscriber vel_sub = ns.subscribe("feedback_Vel", 50, callback);
   ros::Publisher odom_pub = np.advertise<nav_msgs::Odometry>("odom", 50);
 
   tf::TransformBroadcaster odom_broadcaster;
 
-  ros::Time current_time ,last_time;
+  ros::Time current_time, last_time;
   current_time = ros::Time::now();
   last_time = ros::Time::now();
-  
+
   ros::Rate r(100);
 
-  while(np.ok())
-  {
+  while (np.ok()) {
     ros::spinOnce();
     current_time = ros::Time::now();
 
@@ -45,11 +44,11 @@ int main(int argc, char **argv)
     double dx = (vx*cos(th) - vy*sin(th)) * dt;
     double dy = (vx*sin(th) + vy*cos(th)) * dt;
     double dth = vth * dt;
-    
-    x+=dx;
-    y+=dy;
-    th+=dth;
-    
+
+    x  += dx;
+    y  += dy;
+    th += dth;
+
     geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
     //
     geometry_msgs::TransformStamped odom_trans;
@@ -65,23 +64,23 @@ int main(int argc, char **argv)
     odom_broadcaster.sendTransform(odom_trans);
     //
     nav_msgs::Odometry odom;
-    
+
     odom.header.stamp = current_time;
     odom.header.frame_id = "odom";
-    
+
     odom.pose.pose.position.x = x;
     odom.pose.pose.position.y = y;
     odom.pose.pose.position.z = 0.0;
     odom.pose.pose.orientation = odom_quat;
-    
+
     odom.child_frame_id = "base_footprint";
     odom.twist.twist.linear.x = vx;
     odom.twist.twist.linear.y = vy;
     odom.twist.twist.angular.z = vth;
-    
+
     odom_pub.publish(odom);
     last_time = current_time;
-    r.sleep();    
+    r.sleep();
   }
   return 0;
 }
--- lab6/src/pose_subscribe.cpp
+++ lab6/src/pose_subscribe.cpp
@@ -2,40 +2,39 @@
 #include "geometry_msgs/Twist.h"
 #include "tf/tf.h"
 
-double x_now = 0.0;
-double y_now = 0.0;
+double x_now  = 0.0;
+double y_now  = 0.0;
 double th_now = 0.0;
 
 const float PI = 3.14159265358979323;
 
 ros::Subscriber sub;
 
-void GoalCallback(const geometry_msgs::Twist &msg)
-{	
-	geometry_msgs::Quaternion odom_quat;
+void GoalCallback(const geometry_msgs::Twist &msg) {
+    geometry_msgs::Quaternion odom_quat;
 
-	x_now = msg.linear.x;					//unit: m
-	y_now = msg.linear.y;					//unit: m
-	th_now = msg.angular.z;			//quaternoin
+    x_now  = msg.linear.x;          //  unit: m
+    y_now  = msg.linear.y;          //  unit: m
+    th_now = msg.angular.z;         //  quaternoin
 
-	if(th_now>PI){th_now-=2*PI;}
-	else if(th_now<-PI){th_now+=2*PI;}
-	
-	ROS_INFO("x: %.3f /t y: %.3f /t th: %.3f",x_now,y_now,th_now);
+    if (th_now > PI) {
+        th_now -= 2*PI;
+    } else if (th_now < -PI) {
+        th_now += 2*PI;
+    }
+
+    ROS_INFO("x: %.3f /t y: %.3f /t th: %.3f", x_now, y_now, th_now);
 }
 
-int main(int argc, char** argv){
-  ros::init(argc, argv, "pose_subscribe");
- 
-  ros::NodeHandle n;
-  
-  ros::Rate r(100.0);
-  while(n.ok()){
- 
-    ros::spinOnce();               // check for incoming messages
- 
-		sub = n.subscribe("/robot_pose", 10, GoalCallback);
+int main(int argc, char* argv[]) {
+    ros::init(argc, argv, "pose_subscribe");
 
-    r.sleep();
-  }
+    ros::NodeHandle n;
+
+    ros::Rate r(100.0);
+    while (n.ok()) {
+        ros::spinOnce();        // check for incoming messages
+        sub = n.subscribe("/robot_pose", 10, GoalCallback);
+        r.sleep();
+    }
 }
