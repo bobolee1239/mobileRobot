0ab203a17c664c81798064a4cbb7a020edca3b83
Brian Lee <bobolee1239@gmail.com>
2019-05-12 15:21:40 +0800

    add lab6 own navigator -- brian

---
+++ lab6/src/lab6_Navigator.cpp
@@ -0,0 +1,191 @@
+//  Copyright (c) 2019 Tsung-Han Brian Lee
+/*********************************************************
+ ** -------------- Navigator.cpp -----------------
+ **   AUTHOR      : Tsung-Han Brian Lee
+ **   DESCRIPTION : Navigator with A* algorithm
+ ** -----------------------------------------------
+ *********************************************************/
+#include <time.h>                       //  random seed
+#include <stdlib.h>                     //  random walk
+#include "ros/ros.h"                    //  ROS
+#include "ros/console.h"                //  To setup verbosity
+#include "nav_msgs/OccupancyGrid.h"     //  OccupancyGrid msg
+#include "nav_msgs/Odometry.h"          //  Odometry msg
+#include "geometry_msgs/Twist.h"        //  Twist msg
+#include "geometry_msgs/PoseStamped.h"  //  PoseStamped msg
+#include "geometry_msgs/Point.h"        //  Point msg
+#include "tf/tf.h"                      //  Coord Transform
+#include "a-star.h"                     //  My A* algo Library
+/*--------------------------------------------------------------------*/
+#define PI              3.14159265358979323
+#define VEHICLE_WIDTH   0.2             // Unit: meter
+#define RANDOM_STEP     6
+
+/* define topic name as following */
+#define POSE_TOPIC      "/robot_pose"
+#define MAP_TOPIC       "/map"
+#define SUBGOAL_TOPIC   "/subgoal_position"
+/******************************************
+ ** ----- Help Fcn & Structure -----
+ ******************************************/
+typedef geometry_msgs::Point Point;
+
+typedef struct {
+    double x;
+    double y;
+    double th;
+} PlanarInfo;
+
+void handleRecvGoal(const geometry_msgs::PoseStamped& msg);
+void buildMap(const nav_msgs::OccupancyGrid& map);
+void updatePose(const geometry_msgs::Twist& pose);
+/******************************************
+ ** ----- Global Variable -----
+ **   => To be caught in callback fcn
+ ******************************************/
+/* Pointer to our Map */
+Map* myMap = NULL;          //  to be initialized in callback fcn
+volatile PlanarInfo goal;   //  destination and desired pose
+/*--------------------------------------------------------------------*/
+int main(int argc, char* argv[]) {
+    /* random random seed */
+    unsigned int seed = time(NULL);
+
+    /* Init ROS node : navigator */
+    ros::init(argc, argv, "Navigator");
+
+    /* Setup ROS Verbosity Level */
+    if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug)) {
+        ros::console::notifyLoggerLevelsChanged();
+    }
+
+    ros::NodeHandle nh;
+
+    ros::Publisher  pubSubgoal = nh.advertise<Point>(SUBGOAL_TOPIC, 1);
+    ros::Subscriber subPose    = nh.subscribe(POSE_TOPIC, 10, &updatePose);
+    ros::Subscriber subMap     = nh.subscribe(MAP_TOPIC, 10, &buildMap);
+    ros::Subscriber subGoal    = nh.subscribe("/move_base_simple/goal",
+                                              10, &handleRecvGoal);
+
+
+    /**
+     **  subgoal :
+     **     x : x coordinate of destination
+     **     y : y coordinate of destination
+     **     z : vehicle pose, that is the heading direction
+     **         - shall in the range -PI ~ PI
+     **         - 87 for not specified
+     **/
+    geometry_msgs::Point subgoal;       // to be published to SUBGOAL_TOPIC
+    ros::Rate loopRate(100.0);          //  Unit: Hz
+    while (ros::ok()) {
+        ros::spinOnce();                // to be able to fire callback fcn
+
+        /* do nothing if myMap is not yet created */
+        if (myMap == NULL)  continue;
+
+        Node dest(goal.x, goal.y);
+        ROS_DEBUG_STREAM("[Navigator]: Finding path from "
+                          << myMap->dac(*static_cast<Node*>(
+                              myMap->at(myMap->getRobot().getPosition())))
+                          << " -> " << dest);
+        auto path = myMap->aStar(dest);
+        /*
+         std::cout << "Found path: ";
+         for (auto&& node : path) {
+             std::cout << static_cast<Node>(node) << " -> ";
+         }
+         std::cout << std::endl;
+         */
+        if (path.empty()) {
+            ROS_INFO_STREAM("[Navigator]: RANDOM WALK FOR NO PATH FOUND!");
+            subgoal.x = myMap->getRobot().getPosition().getX()
+                + RANDOM_STEP*static_cast<double>(rand_r(&seed)) / RAND_MAX * myMap->getResolution();
+            subgoal.y = myMap->getRobot().getPosition().getY()
+                + RANDOM_STEP*static_cast<double>(rand_r(&seed)) / RAND_MAX * myMap->getResolution();
+            subgoal.z = 87;
+        } else if (path.size() > 2) {
+            subgoal.x = path[2].getX();
+            subgoal.y = path[2].getY();
+            subgoal.z = 87;
+        } else if (path.size() > 1) {
+            subgoal.x = path.back().getX();
+            subgoal.y = path.back().getY();
+            subgoal.z = goal.th;
+        } else {
+            /* we're now in the goal */
+            subgoal.x = myMap->getRobot().getPosition().getX();
+            subgoal.y = myMap->getRobot().getPosition().getY();
+            subgoal.z = goal.th;
+        }
+
+        pubSubgoal.publish(subgoal);
+
+        ROS_INFO_STREAM("[Navigator]: subgoal ("
+                        << subgoal.x << "," << subgoal.y << ")");
+        ROS_INFO_STREAM("[Navigator]: goal ("
+                        << goal.x << "," << goal.y << ")"
+                        << " @ " << goal.th/PI*180.0);
+
+        loopRate.sleep();
+    }
+
+    return 0;
+}
+/*--------------------------------------------------------------------*/
+/******************************************
+ ** ----- Help Fcn Implementation -----
+ ******************************************/
+/**
+ **  Handle user setting goal event
+ **/
+void handleRecvGoal(const geometry_msgs::PoseStamped& msg) {
+    goal.x  = msg.pose.position.x;
+    goal.y  = msg.pose.position.y;
+    /*****************************************************
+     ** ------------------ NOTE --------------------
+     **  1. Theta is transformed from quaternion to
+     **     raw pitch yaw (rpy).
+     **  2. Range: -PI ~ PI
+     **  3. Unit : rad
+     ** ---------------------------------------------
+     *****************************************************/
+    goal.th = tf::getYaw(msg.pose.orientation);
+
+    ROS_DEBUG_STREAM("[Navigator]: Goal (" << goal.x << ", " << goal.y << ") @"
+                     << goal.th/PI*180.0);
+}
+
+/**
+ **  Build & Update our map
+ **/
+void buildMap(const nav_msgs::OccupancyGrid& map) {
+    /**
+     ** ----------------------------------------------
+     **   1. Create our map for the very first time
+     **   2. Update our map if myMap is existed
+     ** ----------------------------------------------
+     **/
+    if (myMap) {
+        myMap->updateMap(map);
+    } else {
+        myMap = new Map(map, VEHICLE_WIDTH);
+    }
+}
+
+/**
+ **  Update the vehicle pose
+ **/
+void updatePose(const geometry_msgs::Twist& pose) {
+    /* do nothing if map is not created yet */
+    if (myMap == NULL)  return;
+
+    /* update postion */
+    myMap->moveRobotTo(pose.linear.x, pose.linear.y);
+    /* update pose */
+    myMap->getRobot().setPose(pose.angular.z);
+
+    ROS_DEBUG_STREAM("[Navigator]: bot pos:" << myMap->dac(*static_cast<Node*>(
+                      myMap->at(myMap->getRobot().getPosition())))
+                      << " @ " << myMap->getRobot().getPose() / PI * 180.0);
+}
---
+++ lab6/src/lab6_nav.cpp
@@ -0,0 +1,172 @@
+//  Copyright (c) 2019 Tommy Huang, Tsung-Han Brian Lee
+/***************************************************************
+ ** ---------------- Mobile Vehicle Control -------------------
+ **   AUTHOR      : Tommy Huang, Tsung-Han Brian Lee
+ **   DESCRIPTION : Revised from Dynamic Systems n Control Lab
+ ** -----------------------------------------------------------
+ ***************************************************************/
+#include <math.h>
+#include "ros/ros.h"
+#include "ros/console.h"
+#include "geometry_msgs/Point.h"
+#include "geometry_msgs/Twist.h"
+#include "nav_msgs/Odometry.h"
+
+#define PI 3.14159265358979323      // circumference ratio.
+
+// [TODO] ... adjusting the control gain !! ...
+const double k_rho      = 1.00; //1.00
+const double k_alpha    = 2.60; // 2.60
+const double k_beta     = 0.0;
+
+/************************************************
+ **  Global variables to allow communication
+ **  between functions
+ ************************************************/
+geometry_msgs::Twist command;
+
+float x_now;
+float y_now;
+float th_now;
+/************************************************
+ **  Callback Functions
+ ************************************************/
+void subgoalCallback(const geometry_msgs::Point &msg);
+void poseCallback(const geometry_msgs::Twist& loc);
+
+int main(int argc, char* argv[]) {
+    /**
+     ** Initializing the ROS nh : "nav"
+     **/
+    ros::init(argc, argv, "nav");
+    ros::NodeHandle nh;
+
+    ros::Publisher pub;
+    ros::Subscriber sub;
+    ros::Subscriber sub2;
+
+    /**
+     ** Publish robot movement
+     **     => advertise topic
+     **/
+    pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+
+    /**
+     ** Subscribe robot pose and the reference signal
+     **     => install callback function
+     **/
+    sub = nh.subscribe("/subgoal_position", 10, subgoalCallback);
+    sub2 = nh.subscribe("/robot_pose", 10, poseCallback);
+
+    /* Looping in Fs : 100 Hz */
+    ros::Rate rate(100);
+
+    while (ros::ok()) {
+        /* Attempting to fire callback */
+        ros::spinOnce();
+
+        /* Publish control command message */
+        pub.publish(command);
+
+        /* constant looping in 100 Hz */
+        rate.sleep();
+    }
+
+    return 0;
+}
+
+void subgoalCallback(const geometry_msgs::Point &msg) {
+    /* initializing command ? */
+    command.linear.x  = 0.0;
+    command.linear.y  = 0.0;
+    command.angular.z = 0.0;
+
+    /*********************************************
+	 ** Apply control law following
+     *********************************************/
+    double xDiff = static_cast<double>(msg.x - x_now);
+    double yDiff = static_cast<double>(msg.y - y_now);
+    double pDiff = static_cast<double>(msg.z - th_now);     //  pose angle diff
+    //  Wrapping the pDiff in the range of -PI ~ PI
+    if (pDiff > PI) {
+      pDiff -= 2*PI;
+    } else if (pDiff < -PI) {
+      pDiff += 2*PI;
+    }
+
+    /**
+     **  Euclidean distance between robot and target
+     **/
+    double distance = sqrt(pow(xDiff, 2) + pow(yDiff, 2));
+    /**
+     **  Angle difference between heading direction and target
+     **/
+    double headingAngleDiff = atan2(yDiff, xDiff) - th_now;
+    /* wrapping the angle between -PI ~ PI */
+    if (headingAngleDiff > PI) {
+        headingAngleDiff -= 2*PI;
+    } else if (headingAngleDiff < -PI) {
+        headingAngleDiff += 2*PI;
+    }
+
+    double linear_vel  = k_rho    * distance;
+    double angular_vel = k_alpha * headingAngleDiff;
+
+    /* [TODO] control beta if */
+    /*
+    if (pDiff - 87 > 0.001) {
+        angular_vel += k_beta * pDiff;
+    }
+    */
+
+    /**
+     ** 1. Saturating linear velocity
+     ** 2. Setting control command
+     **/
+    if (linear_vel > 0.25) {
+        command.linear.x = 0.25;
+    } else if (linear_vel < -0.25) {
+        command.linear.x = -0.25;
+    } else {
+        command.linear.x = linear_vel;
+    }
+    /**
+     ** 1. Saturating angular velocity
+     ** 2. Setting control command
+     **/
+    if (angular_vel > 1.0) {
+        command.angular.z = 1.0;
+    } else if (angular_vel < -1.0) {
+        command.angular.z = -1.0;
+    } else {
+        command.angular.z = angular_vel;
+    }
+    /**
+     **  Handle when close to goal
+     **     => in 1 mm
+     **/
+    if (distance < 0.06) {
+        ROS_INFO_STREAM("[NAV] WE ARE CLOSE ENOUGH!");
+        //  [TODO] it's just a temp way to control pose
+        command.angular.z = pDiff*k_beta;
+        command.linear.x  = 0.0;
+    }
+
+    std::cout << "-----------------------------------\n";
+    ROS_INFO_STREAM("[NAV] x_now: " << x_now);
+    ROS_INFO_STREAM("[NAV] y_now: " << y_now);
+    ROS_INFO_STREAM("[NAV] th_now: " << th_now/PI*180.0);
+    ROS_INFO_STREAM("[NAV] xDiff: " << xDiff);
+    ROS_INFO_STREAM("[NAV] yDiff: " << yDiff);
+    ROS_INFO_STREAM("[NAV] distance: " << distance);
+    ROS_INFO_STREAM("[NAV] heading angle diff: " << headingAngleDiff/PI*180.0);
+    ROS_INFO_STREAM("[NAV] linear_vel: " << linear_vel);
+    ROS_INFO_STREAM("[NAV] angular_vel: " << angular_vel);
+}
+
+void poseCallback(const geometry_msgs::Twist& loc) {
+    //  Update robot position
+    x_now  = loc.linear.x;
+    y_now  = loc.linear.y;
+    th_now = loc.angular.z;
+}
